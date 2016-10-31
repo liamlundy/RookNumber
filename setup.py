665ef15caa27f3b7a7efd96c2d672e1697b0f6e5
Liam Lundy <llundy013@gmail.com>
2016-10-31 16:11:15 -0400

    Added setup script

---
+++ setup.py
@@ -0,0 +1,8 @@
+from distutils.core import setup
+
+setup(name='RookNumber',
+      version='1.0',
+      author='Liam Lundy',
+      author_email='llundy@gmail.com',
+      py_modules=['board'],
+      )
